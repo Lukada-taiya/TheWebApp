bf6b29e6b4cfb56102bfe6abb591cea13e5cec85
Lukada-taiya <lukmanadam43@gmail.com>
2022-11-03 14:27:25 +0000

    First Commit. Generated a base spring boot app. Created two jpa entities Book and Author with a many to many to many relationship

---
+++ src/test/java/com/lutadam/TheWebApp/TheWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lutadam.TheWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
